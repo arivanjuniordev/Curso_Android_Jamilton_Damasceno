606217e7a90c7bdab61964f869df7516b5df4d5d
arivanjuniordev <94536010+arivanjuniordev@users.noreply.github.com>
2024-07-01 12:01:29 -0300

    first app

---
+++ AppSortearNumero/app/src/main/java/br/com/arivanjuniordev/appsortearnumero/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.com.arivanjuniordev.appsortearnumero
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sortition(view: View){
+        val valueRandom = Random().nextInt(11)
+        val result = findViewById<TextView>(R.id.textView_result)
+        result.setText("Número gerado $valueRandom")
+    }
+}
